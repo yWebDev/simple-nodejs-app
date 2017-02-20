2428f068ec0da34fe9f541dcf534936bf9aa4af6
yshpakovych <yshpakovych@exadel.com>
2017-02-20 18:00:17 +0200

    Add db creation

---
+++ models/User.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const log = require('libs/log')(module);
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+userSchema.methods.encryptPassword = function(password) {
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+};
+
+userSchema.virtual('password')
+  .set(function (password) {
+    this._plainPassword = password;
+    this.salt = Math.random() + 'salt';
+    this.hashedPassword = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._plainPassword;
+  });
+
+userSchema.methods.checkPassword = function(password) {
+  return this.encryptPassword(password) === this.hashedPassword
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const engine = require('ejs-locals');
 const path = require('path');
 const config = require('config');
 const log = require('libs/log')(module);
-const db = require('db/connect');
+const loggerFormat = require('libs/loggerFormat');
 const app = express();
 
 app.engine('ejs', engine);
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(express.favicon());
 
 if (app.get('env') === 'development') {
-  app.use(express.logger('debug'));
+  app.use(express.logger('dev', loggerFormat));
 } else {
   app.use(express.logger('default'));
 }
@@ -25,13 +25,9 @@ app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Node App'
-  })
+app.use((req, res) => {
+  res.send(404, 'Page Not Found');
 });
-
 app.use((err, req, res, next) => {
   if (app.get('env') === 'development') {
     const errorHandler = express.errorHandler();
@@ -39,6 +35,12 @@ app.use((err, req, res, next) => {
   } else {
     res.send(500);
   }
+});
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Node App'
+  })
 });
 
 app.listen(config.get('port'), () => {
