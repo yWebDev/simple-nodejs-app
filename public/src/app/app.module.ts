9350624bed76303f460809772d41ec764e0e4c7d
yshpakovych <yshpakovych@exadel.com>
2017-02-22 13:49:23 +0200

    Create login, app-wrapper, user-service

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,19 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserService } from './services/user/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginPageComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
